94ec102661a50daaeb40c6910e42ce5de42da58b
Nonato Home <nonato.developer@gmail.com>
2018-06-16 23:21:21 -0300

    criacao de arquivos e importação de imagens

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const MDCFoo = mdc.foo.MDCFoo;
+const MDCFooFoundation = mdc.foo.MDCFooFoundation;
+
+const countries = [
+    'França', 
+    'Austrália', 
+    'Argentina', 
+    'Islândia', 
+    'Peru', 
+    'Dinamarca', 
+    'Croácia', 
+    'Nigéria', 
+    'Costa Rica', 
+    'Sérvia', 
+    'Alemanha', 
+    'México', 
+    'Rússia', 
+    'Arábia Saudita', 
+    'Egito', 
+    'Uruguai', 
+    'Marrocos', 
+    'Irã', 
+    'Portugal', 
+    'Espanha', 
+    'Brasil', 
+    'Suíça', 
+    'Suécia', 
+    'Coreia do Sul', 
+    'Bélgica', 
+    'Panamá', 
+    'Tunísia', 
+    'Inglaterra', 
+    'Colômbia', 
+    'Japão', 
+    'Polônia', 
+    'Senegal'
+]
