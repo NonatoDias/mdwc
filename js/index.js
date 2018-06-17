808f7221fea29de37f6d7cdefbbcc9eaa5500ae6
Nonato Home <nonato.developer@gmail.com>
2018-06-17 00:03:46 -0300

    jquery

--- js/index.js
+++ js/index.js
@@ -1,37 +1,55 @@
-const MDCFoo = mdc.foo.MDCFoo;
-const MDCFooFoundation = mdc.foo.MDCFooFoundation;
+/**
+ * Js 
+ * @author Nonato Dias
+ */
 
-const countries = [
-    'França', 
-    'Austrália', 
-    'Argentina', 
-    'Islândia', 
-    'Peru', 
-    'Dinamarca', 
-    'Croácia', 
-    'Nigéria', 
-    'Costa Rica', 
-    'Sérvia', 
-    'Alemanha', 
-    'México', 
-    'Rússia', 
-    'Arábia Saudita', 
-    'Egito', 
-    'Uruguai', 
-    'Marrocos', 
-    'Irã', 
-    'Portugal', 
-    'Espanha', 
-    'Brasil', 
-    'Suíça', 
-    'Suécia', 
-    'Coreia do Sul', 
-    'Bélgica', 
-    'Panamá', 
-    'Tunísia', 
-    'Inglaterra', 
-    'Colômbia', 
-    'Japão', 
-    'Polônia', 
-    'Senegal'
-]
+
+
+(function($){
+
+    /* =============  consts ============= */
+    const countries = [
+        'França', 
+        'Austrália', 
+        'Argentina', 
+        'Islândia', 
+        'Peru', 
+        'Dinamarca', 
+        'Croácia', 
+        'Nigéria', 
+        'Costa Rica', 
+        'Sérvia', 
+        'Alemanha', 
+        'México', 
+        'Rússia', 
+        'Arábia Saudita', 
+        'Egito', 
+        'Uruguai', 
+        'Marrocos', 
+        'Irã', 
+        'Portugal', 
+        'Espanha', 
+        'Brasil', 
+        'Suíça', 
+        'Suécia', 
+        'Coreia do Sul', 
+        'Bélgica', 
+        'Panamá', 
+        'Tunísia', 
+        'Inglaterra', 
+        'Colômbia', 
+        'Japão', 
+        'Polônia', 
+        'Senegal'
+    ]
+
+
+    /* =============  model ============= */
+
+    let model = {
+        user: ''
+    }
+
+
+
+})(jQuery);
---
+++ sw.js
@@ -0,0 +1,67 @@
+/**
+ * Service worker
+ * @author Nonato Dias
+ * @version 1.0
+ */
+
+
+var CACHE_VERSION = 13;
+var CACHE_NAME = 'mdwc-static-v'+CACHE_VERSION;
+var OFFLINE_URL = '';
+
+//dsd
+var urlsToCache = [
+    '/libs/jquery/jquery-3.2.1.min.js'
+]
+
+self.addEventListener('install', function(event) {
+    //self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('mdwc-') && cacheName != CACHE_NAME;
+                
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+
+    //Evento somente para mesma origem; Skip cross-origin requests
+    //Evento com arquivos estáticos
+    if (event.request.url.startsWith(self.location.origin) && event.request.url.indexOf("/ws-portal/") === -1) {
+        if( !(event.request.mode === "no-cors" && event.request.cache === "only-if-cached")){
+                event.respondWith(
+                caches.match(event.request).then(function(response) {
+                    return response || fetch(event.request).then(function(fetch_resp){
+                        //console.log(fetch_resp.url);
+                        return fetch_resp;
+                    });
+                })
+            );
+        }
+    }
+});
+
+self.addEventListener('message', function(event) {
+    if (event.data.actionsw === 'SKIPWAITING') {
+        self.skipWaiting().then(function(t) {
+
+            console.log("pulou a atualizacao", t);
+        });
+    }
+});
